3ace4892b85ce9f6977f0d33b567dbba2b681c57
apadilah30 <aguspadilah30@gmail.com>
2026-05-18 08:55:23 +0700

    fix: build failed

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  i18n: {
-    locales: ["en", "id"],
-    defaultLocale: "en",
-  },
 };
 
 export default nextConfig;
